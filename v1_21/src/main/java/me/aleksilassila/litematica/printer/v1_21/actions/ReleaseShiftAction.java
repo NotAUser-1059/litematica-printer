69539df6e6997ed8d834b6bca95eefb887bd5aa9
Not A User <88565974+NotAUser-1059@users.noreply.github.com>
2026-04-14 16:12:20 -0500

    Replace Input with PlayerInput in ReleaseShiftAction

--- v1_21/src/main/java/me/aleksilassila/litematica/printer/v1_21/actions/ReleaseShiftAction.java
+++ v1_21/src/main/java/me/aleksilassila/litematica/printer/v1_21/actions/ReleaseShiftAction.java
@@ -1,6 +1,6 @@
 package me.aleksilassila.litematica.printer.v1_21.actions;
 
-import net.minecraft.entity.player.Input;
+import net.minecraft.util.PlayerInput;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.network.packet.c2s.play.PlayerInputC2SPacket;
@@ -8,6 +8,6 @@ import net.minecraft.network.packet.c2s.play.PlayerInputC2SPacket;
 public class ReleaseShiftAction extends Action {
     @Override
     public void send(MinecraftClient client, ClientPlayerEntity player) {
-        player.networkHandler.sendPacket(new PlayerInputC2SPacket(new Input(false, false, false, false, false, false, false)));
+        player.networkHandler.sendPacket(new PlayerInputC2SPacket(new PlayerInput(false, false, false, false, false, false, false)));
     }
 }
